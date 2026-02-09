833caee65e1aa3e6a23ab6e10cfa7d0dce559eb6
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2026-02-09 14:13:53 +0000

    aula1

---
+++ ola.py
@@ -0,0 +1,8 @@
+#nome = input("Digite seu nome: ")
+#print("Olá, " , nome , "Seja Bem Vindo")
+
+idade = input("Qual a sua idade? ")
+nome = input("Qual o seu nome? ")
+print("Entendi,sua idade é", idade , "Que legal, E seu nome é", nome)
+
+    
