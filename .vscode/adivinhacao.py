2840e2a12ce24ecc3e054eca32cc09452b8540cb
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2026-02-24 12:08:43 +0000

    vercao02

--- .vscode/adivinhacao.py
+++ .vscode/adivinhacao.py
@@ -3,14 +3,29 @@ print('*******Jogo de Adivinhação*******')
 print('*********************************')
 
 numero_secreto = 39
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite seu número: ")
 
-print("Seu número é: ", chute_str)
 
-chute = int(chute_str)
+while (rodada <= total_tentativas):
 
-if(numero_secreto == chute_str):
-    print("Você acertou! ")
-else:
-    print("Você errou  :( ")
+    chute_str = input("Digite seu número: ")
+    print("Seu número é: ", chute_str)
+
+    chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou! ")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior que o número secreto")
+        elif(menor):
+            print("O seu chute foi menor que o número secreto")
+    rodada = rodada +1
+
+print("Fim de jogo!")
